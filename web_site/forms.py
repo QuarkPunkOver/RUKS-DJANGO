43c3f68e999127f35fcc919fdd319aadac357e32
funDAVEover <111georgiy111@gmail.com>
2023-12-07 21:16:36 +0300

    Add to git

---
+++ web_site/forms.py
@@ -0,0 +1,60 @@
+from django import forms
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordResetForm, SetPasswordForm
+from django.utils.translation import ugettext_lazy as _
+from snowpenguin.django.recaptcha3.fields import ReCaptchaField
+from django.utils.translation import gettext, lazy
+from web_site.models import Reviews
+
+
+class ReviewForm(forms.ModelForm):
+    captcha = ReCaptchaField()
+
+    class Meta:
+        model = Reviews
+        fields = ['name', 'text', 'rating', 'captcha']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form__input', 'placeholder': 'Ваше Имя'}),
+            'text': forms.Textarea(
+                attrs={'class': 'form__textarea', 'id': "contactcomment", 'placeholder': 'Ваш отзыв'}),
+        }
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': "sign__input", 'placeholder': 'Email'}))
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': "sign__input", 'placeholder': 'Имя пользователя'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': "sign__input", 'placeholder': 'Пароль'}))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': "sign__input", 'placeholder': 'Подтвердите пароль'}))
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'sign__input', 'placeholder': 'Имя пользователя'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'sign__input', 'placeholder': 'Пароль'}))
+
+
+class ResetPasswordForm(PasswordResetForm):
+    email = forms.EmailField(
+        label=_("Email"),
+        max_length=254,
+        widget=forms.EmailInput(
+            attrs={'autocomplete': 'email', 'class': 'sign__input', 'placeholder': _('Enter your Email')})
+    )
+
+
+class SetNewPasswordForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label=_("New password"),
+        widget=forms.PasswordInput(
+            attrs={'autocomplete': 'new-password', 'class': 'sign__input', 'placeholder': _('New password')}),
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    new_password2 = forms.CharField(
+        label=_("New password confirmation"),
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={'autocomplete': 'new-password', 'class': 'sign__input', 'placeholder': _('Repeat password')}),
+    )
---
+++ web_site/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from django.views.decorators.cache import cache_page
+
+from .views import *
+
+urlpatterns = [
+    path('', cache_page(60 * 10)(MoviesView.as_view()), name='home'),
+    path('catalog/<int:page>/', cache_page(60 * 10)(CatalogView.as_view()), name='catalog'),
+    path('filter/', cache_page(60 * 10)(FilterMoviesView.as_view()), name='filter'),
+    path('search/', cache_page(60 * 10)(Search.as_view()), name='search'),
+    path('about/', cache_page(60 * 10)(about_us), name='about'),
+    path('help/', cache_page(60 * 10)(faq_page), name='help_page'),
+    path('termsofuse/', cache_page(60 * 10)(terms_page), name='terms_of_use'),
+    path('register/', cache_page(60 * 10)(UserRegisterView.as_view()), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('catalog/<slug:slug>/<int:page>/', cache_page(60 * 10)(CatalogView.as_view()), name='genre_catalog'),
+    path(r'<slug:slug>/review/<int:pk>/like/', VotesView.as_view(
+        model=Reviews, vote_type=LikeDislike.LIKE), name='review_like'),
+    path(r'<slug:slug>/review/<int:pk>/dislike/', VotesView.as_view(
+        model=Reviews, vote_type=LikeDislike.DISLIKE), name='review_dislike'),
+    path('<slug:slug>/', SingleMovieView.as_view(), name='movie_detail'),
+    path("review/<int:pk>/", AddReview.as_view(), name="add_review"),
+    path("actor/<str:slug>/<int:page>/", cache_page(60 * 10)(ActorDetailView.as_view()), name="actor_detail"),
+    path("producer/<str:slug>/<int:page>/", cache_page(60 * 10)(DirectorDetailView.as_view()), name="director_detail"),
+]
