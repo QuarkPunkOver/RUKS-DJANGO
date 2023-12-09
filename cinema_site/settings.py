f976f256c49e7421c472df5013cb5f4087910600
funDAVEover <111georgiy111@gmail.com>
2023-12-09 20:12:33 +0300

    preapre to upload

--- cinema_site/settings.py
+++ cinema_site/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 ALLOWED_HOSTS = []
 DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 # Application definition
 DATABASES = {
     'default': {
